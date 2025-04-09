e44a4cdfbe68f30de74c4013a07f91482ef0cbdd
B4tiste <batiste.laloi@gmail.com>
2025-04-09 13:37:31 +0200

    fix

--- src/commands/shared/player_alias.rs
+++ src/commands/shared/player_alias.rs
@@ -1,35 +1,16 @@
-use std::{collections::HashMap, fs};
-use serde::Deserialize;
+use std::collections::HashMap;
 use lazy_static::lazy_static;
-
-#[derive(Debug, Deserialize)]
-pub struct PlayerAliasList {
-    players: Vec<PlayerAlias>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct PlayerAlias {
-    // og_name: String,
-    en_names: Vec<String>,
-    // swarena_id: i64,
-    swrt_id: i64,
-}
 
 lazy_static! {
     pub static ref PLAYER_ALIAS_MAP: HashMap<i64, String> = {
-        let file_content = fs::read_to_string(format!(
-            "{}/src/commands/shared/player_alias.json",
-            env!("CARGO_MANIFEST_DIR")
-        ))
-        .expect("Failed to read player_alias.json");
+        let mut m = HashMap::new();
 
-        let alias_list: PlayerAliasList = serde_json::from_str(&file_content)
-            .expect("Failed to parse player_alias.json");
+        m.insert(54175, "Kelianbao".to_string());
+        m.insert(48169, "tars".to_string());
+        m.insert(30389, "Lest".to_string());
+        m.insert(8123, "sk!t".to_string());
+        m.insert(11026, "cabrera".to_string());
 
-        alias_list
-            .players
-            .into_iter()
-            .filter_map(|p| p.en_names.get(0).map(|alias| (p.swrt_id, alias.clone())))
-            .collect()
+        m
     };
 }
