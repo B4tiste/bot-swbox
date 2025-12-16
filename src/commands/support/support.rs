30fb16c02e9d6f32adfa62884677235c7b1f0d65
B4tiste <batiste.laloi@gmail.com>
2025-12-16 16:28:37 +0100

    Edit /support text

--- src/commands/support/support.rs
+++ src/commands/support/support.rs
@@ -14,9 +14,9 @@ pub async fn support(ctx: poise::ApplicationContext<'_, Data, Error>) -> Result<
     let embed = serenity::CreateEmbed::default()
         .title("Support the Project")
         .color(serenity::Colour::from_rgb(0, 255, 255))
-        .description("If you like this bot and want to support the project, consider donating on **[Ko-Fi](https://ko-fi.com/swbox)**.")
+        .description("As of Friday 19th of December 2025, the hosting service used to keep the BOT online has removed their free tier. Therefore, the project now needs money to run. If you like this bot and want to support the project, please consider donating on **[Ko-Fi](https://ko-fi.com/swbox)**.")
         .field(
-            "Commission : Custom Player Alias (3€)",
+            "Custom Commission : Custom Player Alias (3€)",
             "You can add a custom player alias for yourself or someone else. This alias can be used to search for that player using the command, and will also be displayed on the player profile.",
             false,
         )
