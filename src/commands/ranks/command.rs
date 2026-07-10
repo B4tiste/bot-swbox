d682b2f87092d521551ac201e9178bbf7e1948af
B4tiste <batiste.laloi@gmail.com>
2026-07-10 09:26:51 +0200

    Fix season end date to 19/09/2026 07:00:00 (UTC)

--- src/commands/ranks/command.rs
+++ src/commands/ranks/command.rs
@@ -95,7 +95,7 @@ pub async fn get_ranks(ctx: poise::ApplicationContext<'_, Data, Error>) -> Resul
 
     // Exact season end date/time provided by maintainer: 26/09/2026 07:00:00 (UTC)
     let date = chrono::Utc
-        .with_ymd_and_hms(2026, 9, 26, 7, 0, 0)
+        .with_ymd_and_hms(2026, 9, 19, 7, 0, 0)
         .single()
         .expect("Invalid hardcoded season end date");
 
